87f8eb0d9937cc48b3fee9a5d932506d7a672546
Jayin Ton <tonjayin@gmail.com>
2015-06-26 01:45:20 +0800

    item: format the time & beautify UI

--- src/app/wap/script/component/EventList/main.jsx
+++ src/app/wap/script/component/EventList/main.jsx
@@ -12,12 +12,17 @@ module.exports = React.createClass({
     },
     render: function(){
         var createItem = function(item){
+            var formatDate = function(data_string){
+                var d = new Date(data_string);
+                return d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
+            }
+            item.create_time = formatDate(item.create_time)
             return (
                 <div onClick={this.handleItemClick.bind(this, item)} className="item">
-                    <div>{item.category}</div>
-                    <div>{item.action}</div>
-                    <div>{item.ip}</div>
-                    <div>{item.create_time}</div>
+                    <div className="col-category">{item.category}</div>
+                    <div className="col-action">{item.action}</div>
+                    <div className="col-ip">{item.ip}</div>
+                    <div className="col-create_time">{item.create_time}</div>
                 </div>
             );
 
--- src/app/wap/script/component/WebsiteList/main.jsx
+++ src/app/wap/script/component/WebsiteList/main.jsx
@@ -13,6 +13,11 @@ module.exports = React.createClass({
     },
     render: function(){
         var createItem = function(item){
+            var formatDate = function(data_string){
+                var d = new Date(data_string);
+                return d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
+            }
+            item.create_time = formatDate(item.create_time)
             return (
                 <div onClick={this.handleItemClick.bind(this, item)} className="item" data-appkey={item.app_key}>
                     <div className="time">{item.create_time}</div>
